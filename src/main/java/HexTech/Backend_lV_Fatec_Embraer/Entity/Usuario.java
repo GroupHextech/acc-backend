96e02e5ba1eca0c0ad8d6f7f8f9db35307e1b86f
Danilo Verginio <zoltancao@gmail.com>
2023-04-01 00:34:35 +0000

    Mapenado a tabela usuario. entity

---
+++ src/main/java/HexTech/Backend_lV_Fatec_Embraer/Entity/Usuario.java
@@ -0,0 +1,41 @@
+package HexTech.Backend_lV_Fatec_Embraer.Entity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table (name ="usr_usuario")
+
+public class Usuario {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column (name = "usr_id")
+private long id;
+
+@Column (name = "usr_nome")
+private String nome;
+
+@Column (name = "usr_senha")
+private String senha;
+
+
+public Usuario (String nome, String senha){
+    this.nome = nome;
+    this.senha= senha;
+}
+
+
+public Usuario() {
+    //Preciso de um método contrutor vazio pois alterei o padrão contrutor padrão
+}
+
+    
+}
+
+
+
