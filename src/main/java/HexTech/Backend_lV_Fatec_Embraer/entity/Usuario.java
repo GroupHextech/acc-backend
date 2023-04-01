00a7751dcd7d3e51a4522027e1d9f60bda84bd29
Danilo Verginio <zoltancao@gmail.com>
2023-04-01 01:20:36 +0000

    Criação do Service e get e set do Usuario

--- src/main/java/HexTech/Backend_lV_Fatec_Embraer/entity/Usuario.java
+++ src/main/java/HexTech/Backend_lV_Fatec_Embraer/entity/Usuario.java
@@ -34,6 +34,39 @@ public Usuario() {
     //Preciso de um método contrutor vazio pois alterei o padrão contrutor padrão
 }
 
+
+public long getId() {
+    return id;
+}
+
+
+public void setId(long id) {
+    this.id = id;
+}
+
+
+public String getNome() {
+    return nome;
+}
+
+
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+
+public String getSenha() {
+    return senha;
+}
+
+
+public void setSenha(String senha) {
+    this.senha = senha;
+}
+
+
+
+
     
 }
 
