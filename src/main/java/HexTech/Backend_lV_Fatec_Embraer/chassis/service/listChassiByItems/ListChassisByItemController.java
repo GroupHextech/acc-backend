1e6c7867a5b3f7e6e6cfc6417e22a2e79de318f6
unknown <lfborgesrp1@gmail.com>
2023-05-30 20:51:38 -0300

    Adjust permission and method that return the user loged

--- src/main/java/HexTech/Backend_lV_Fatec_Embraer/chassis/service/listChassiByItems/ListChassisByItemController.java
+++ src/main/java/HexTech/Backend_lV_Fatec_Embraer/chassis/service/listChassiByItems/ListChassisByItemController.java
@@ -8,23 +8,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import HexTech.Backend_lV_Fatec_Embraer.securityconfig.UserSession;
+import HexTech.Backend_lV_Fatec_Embraer.user.entity.Users;
 
 @RestController
 @RequestMapping(value = "/item/listchassi/{item}")
 @CrossOrigin
 public class ListChassisByItemController {
 
+
+	
 	@Autowired
 	ListChassisByItemService byItemService;
 	
 	@CrossOrigin
 	@GetMapping
-	public ResponseEntity<List<Long>> listChassisByItem(@PathVariable("item") String item){
-		System.out.println("bateu aqui");
+	public ResponseEntity<List<Long>> listChassisByItem(@PathVariable("item") String item, @RequestHeader("token") String token){
+		
+
 		List<Long> chassisIds = byItemService.execute(item);
-		System.out.println("bateu aqui 2");
 
 		return ResponseEntity.status(HttpStatus.OK).body(chassisIds);
 		
